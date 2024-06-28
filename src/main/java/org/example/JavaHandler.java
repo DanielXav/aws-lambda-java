e1af4e87bf75a778dd8932c433691116b5e643b5
Daniel Xavier <dxavier194@gmail.com>
2024-06-28 10:43:14 -0300

    ajuste para realizar uma soma

--- src/main/java/org/example/JavaHandler.java
+++ src/main/java/org/example/JavaHandler.java
@@ -5,16 +5,17 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.Map;
 
-public class JavaHandler implements RequestHandler<Map<String, String>, String> {
+public class JavaHandler implements RequestHandler<Map<String, Integer>, String> {
 
     @Override
     /*
      * Takes in an InputRecord, which contains two integers and a String.
      * Logs the String, then returns the sum of the two Integers.
      */
-    public String handleRequest(Map<String, String> event, Context context)
+    public String handleRequest(Map<String, Integer> event, Context context)
     {
         context.getLogger().log("Evento recebido: " + event);
-        return event.get("name");
+        int soma = event.get("num1") + event.get("num2");
+        return String.valueOf(soma);
     }
 }
